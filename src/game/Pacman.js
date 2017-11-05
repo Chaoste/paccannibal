03870a9d1b10c9ed04431bf273ba412c255155e6
Thomas Kellermeier <thomas.kellermeier@nexenio.com>
2017-11-05 14:21:58 +0100

    Add pacman modi, make feed single

--- src/game/Pacman.js
+++ src/game/Pacman.js
@@ -9,9 +9,28 @@ class Pacman extends Component {
     modus: modi.NONE,
   }
 
+  getCSSModus() {
+    switch(this.state.modus) {
+      case modi.NONE:
+        return 'none'
+      case modi.UP:
+        return 'up'
+      case modi.LEFT:
+        return 'left'
+      case modi.DOWN:
+        return 'down'
+      case modi.RIGHT:
+        return 'right'
+      case modi.DYING:
+        return 'dying'
+      default:
+        return
+    }
+  }
+
   render() {
     return (
-      <div className="pacman">
+      <div className={`pacman ${this.getCSSModus()}`}>
         <div className="pacman-top"></div>
         <div className="pacman-bottom"></div>
       </div>
