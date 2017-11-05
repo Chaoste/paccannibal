305e4c1e5dbb58176f7ee5a1fb00a54b1d35aad1
Thomas Kellermeier <thomas.kellermeier@nexenio.com>
2017-11-05 16:42:33 +0100

    control pacman

--- src/game/Pacman.js
+++ src/game/Pacman.js
@@ -4,14 +4,46 @@ import './Pacman.css';
 import * as modi from './PacmanModi.js';
 import { KEY_CODES } from './Keys.js';
 
+const FPS = 30;
+
 /* Source: https://codepen.io/wifi/pen/olKxE */
 class Pacman extends Component {
   state = {
     modus: modi.NONE,
+    x: 25,
+    y: 38,
   }
+
+  timer = undefined;
 
   componentDidMount = () => {
     window.onkeyup = this.handleKeyPress;
+  }
+
+  changePosition = (x, y) => {
+    this.setState({
+      x: this.state.x + x,
+      y: this.state.y + y,
+    });
+    return true;
+  }
+
+  moving = () => {
+    this.timer = setTimeout(this.moving, 1000 / FPS);
+    switch(this.state.modus) {
+      case modi.LEFT:
+        return this.changePosition(-4, 0);
+      case modi.RIGHT:
+        return this.changePosition(4, 0);
+      case modi.UP:
+        return this.changePosition(0, -4);
+      case modi.DOWN:
+        return this.changePosition(0, 4);
+      case modi.NONE:
+      default:
+        clearTimeout(this.timer);
+        return;
+    }
   }
 
   translateKeyCode = (event) => {
@@ -25,14 +57,19 @@ class Pacman extends Component {
       case KEY_CODES.ARROW_DOWN:
         return modi.DOWN;
       default:
-        return undefined;
+        return modi.NONE;
     }
   }
 
   handleKeyPress = (event) => {
+    const oldModus = this.state.modus;
     this.setState({
       modus: this.translateKeyCode(event) || this.state.modus,
-    })
+    });
+    if (this.state.modus !== oldModus) {
+      clearTimeout(this.timer);
+      this.moving();
+    }
   }
 
   getCSSModus() {
@@ -56,7 +93,14 @@ class Pacman extends Component {
 
   render() {
     return (
-      <div className={`pacman ${this.getCSSModus()}`} onKeyDown={this.handleKeyPress}>
+      <div
+        className={`pacman ${this.getCSSModus()}`}
+        onKeyDown={this.handleKeyPress}
+        style={{
+          left: `${this.state.x}px`,
+          top: `${this.state.y}px`,
+        }}
+      >
         <div className="pacman-top"></div>
         <div className="pacman-bottom"></div>
       </div>
