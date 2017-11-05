ae5a049fac7ad931f709a2309e9e8706dc51972f
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-11-05 11:20:45 +0100

    was hast du gemacht

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import salami from './009-salami.png';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={salami} className="App-logo" alt="salami" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
