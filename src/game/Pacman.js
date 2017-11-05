252eab7b9be6e201fe53a557e4c6822afb8db9f0
Thomas Kellermeier <thomas.kellermeier@nexenio.com>
2017-11-05 18:00:17 +0100

    dying animation

--- src/game/Pacman.js
+++ src/game/Pacman.js
@@ -24,7 +24,7 @@ class Pacman extends Component {
     return x < -15 || y < -15 || y > 113;
   }
 
-  hittingGhots = (x, y) => {
+  hittingGhost = (x, y) => {
     const ghost = document.querySelector('.ghost');
     const rect = this.node.getBoundingClientRect();
     const centerX = rect.left + rect.width / 2;
@@ -32,7 +32,7 @@ class Pacman extends Component {
     const ghostRect = ghost.getBoundingClientRect();
     const ghostX = ghostRect.left + ghostRect.width / 2;
     const ghostY = ghostRect.top + ghostRect.height / 2;
-    return centerX - ghostX < 20 || centerY - ghostY < 20;
+    return Math.abs(centerX - ghostX) < 20 && Math.abs(centerY - ghostY) < 20;
   }
 
   die = () => {
@@ -44,8 +44,10 @@ class Pacman extends Component {
   changePosition = (x, y) => {
     if (this.hittingWall(this.state.x + x, this.state.y + y))
       return;
-    if (this.hittingGhots(this.state.x + x, this.state.y + y))
+    if (this.hittingGhost(this.state.x + x, this.state.y + y)) {
       this.die();
+      return;
+    }
     this.setState({
       x: this.state.x + x,
       y: this.state.y + y,
