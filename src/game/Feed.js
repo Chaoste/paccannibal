23fb57bf832b201ff0316b330b8d3f423f73847f
Thomas Kellermeier <thomas.kellermeier@nexenio.com>
2017-11-05 17:27:12 +0100

    obstacle detection and eating shit

--- src/game/Feed.js
+++ src/game/Feed.js
@@ -5,10 +5,50 @@ import meat2 from '../res/008-kebab.png';
 import meat3 from '../res/012-steak.png';
 import meat4 from '../res/013-fish.png';
 
+import * as modi from './PacmanModi.js';
+
 const images = [meat, meat2, meat3, meat4];
 
 class Feed extends Component {
   mySrc = undefined;
+  node = undefined;
+  state = {
+    hidden: false,
+  }
+
+  getCenter() {
+    const bounds = this.node.getBoundingClientRect();
+    return {
+      x: bounds.left + bounds.width / 2,
+      y: bounds.top + bounds.height / 2,
+    }
+  }
+
+  eatsMe(x, y, dir) {
+    const xDiff = this.getCenter().x - x;
+    const yDiff = this.getCenter().y - y;
+    if (Math.abs(xDiff) < 40 && Math.abs(yDiff) < 40) {
+      if (dir === modi.RIGHT && xDiff > 0 && Math.abs(yDiff) < 30) {
+        return true;
+      } else if (dir === modi.LEFT && xDiff < 0 && Math.abs(yDiff) < 30) {
+        return true;
+      } else if (dir === modi.DOWN && yDiff > 0 && Math.abs(xDiff) < 30 ) {
+        return true;
+      } else if (dir === modi.UP && yDiff < 0 && Math.abs(xDiff) < 30 ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.state.hidden) return;
+    if (this.eatsMe(nextProps.x, nextProps.y, nextProps.dir)) {
+      this.setState({
+        hidden: true,
+      })
+    }
+  }
 
   constructor() {
     super();
@@ -16,7 +56,7 @@ class Feed extends Component {
   }
 
   render() {
-    return <img className="feed" src={this.mySrc} />;
+    return <img className={`feed ${this.state.hidden ? 'hidden' : ''}`} src={this.mySrc} ref={n => this.node = n} />
   }
 }
 
--- src/game/Level.js
+++ src/game/Level.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 
 import Feed from './Feed.js';
+import Pacman from './Pacman.js';
+import * as modi from './PacmanModi.js';
 
 import './Level.css';
 
 class Level extends Component {
+  state = {
+    x: 25,
+    y: 38,
+    dir: modi.NONE,
+  };
+
+  hasMoved = (x, y, dir) => {
+    this.setState({ x, y, dir });
+  }
   render() {
+    const { x, y, dir } = this.state;
     return (
       <div className="level">
-        <Feed />
-        <Feed />
-        <Feed />
-        <Feed />
-        <Feed />
-        <Feed />
-        <Feed />
-
-        {this.props.children}
+        <Feed x={x} y={y} dir={dir} />
+        <Feed x={x} y={y} dir={dir} />
+        <Feed x={x} y={y} dir={dir} />
+        <Feed x={x} y={y} dir={dir} />
+        <Feed x={x} y={y} dir={dir} />
+        <Feed x={x} y={y} dir={dir} />
+        <Feed x={x} y={y} dir={dir} />
+        <Pacman hasMoved={this.hasMoved} />
       </div>
     );
   }
