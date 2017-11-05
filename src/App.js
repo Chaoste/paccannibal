c6880c99278c5c8e970b1478ae8de14d2b86b760
System Administrator <root@Fawazs-MacBook-Pro.local>
2017-11-05 11:36:07 +0100

    Button hinzugefügt

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  onClick = (event) => {
+    console.log(event)
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +17,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="buttoncontainer">
+          <button className="start" onClick={this.onClick}>Start</button>
+        </div>
       </div>
     );
   }
